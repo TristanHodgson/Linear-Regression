76e48ff27a6eb5c7dec8ec4b8d8d60e3eeeb2e07
Tristan Hodgson <hello@tristanhodgson.com>
2025-03-11 16:00:45 +0000

    Linting files

--- data_transform.py
+++ data_transform.py
@@ -4,16 +4,21 @@ tb_path = "./data/incidence-of-tuberculosis-sdgs.csv"
 life_path = "./data/life-expectancy.csv"
 final_path = "./data/data.csv"
 
+
 def Process():
     tb_data = pd.read_csv(tb_path)
     life_expectancy_data = pd.read_csv(life_path)
-    tb_data.rename(columns={"Estimated incidence of all forms of tuberculosis": "TB Cases"}, inplace=True)
-    life_expectancy_data.rename(columns={"Period life expectancy at birth - Sex: total - Age: 0": "Life Expectancy"}, inplace=True)
+    tb_data.rename(columns={
+                   "Estimated incidence of all forms of tuberculosis": "TB Cases"}, inplace=True)
+    life_expectancy_data.rename(columns={
+                                "Period life expectancy at birth - Sex: total - Age: 0": "Life Expectancy"}, inplace=True)
 
-    merged_data = pd.merge(tb_data, life_expectancy_data, on=["Entity", "Year"], how="inner")
+    merged_data = pd.merge(tb_data, life_expectancy_data, on=[
+                           "Entity", "Year"], how="inner")
     merged_data = merged_data[["TB Cases", "Life Expectancy"]].dropna()
 
     merged_data.to_csv(final_path, index=False)
+
 
 if __name__ == "__main__":
     Process()
--- main.py
+++ main.py
@@ -9,16 +9,17 @@ from sklearn.metrics import mean_squared_error
 data_path = "./data/data.csv"
 data = pd.read_csv(data_path)
 
+
 def line(a, b, x):
     return a*x+b
-
 
 
 def LR_Model(data, h1, h2, graph=False):
     x = data[h1].values.reshape(-1, 1)
     y = data[h2].values
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=100)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.1, random_state=100)
 
     model = LinearRegression()
     model.fit(x_train, y_train)
@@ -27,11 +28,12 @@ def LR_Model(data, h1, h2, graph=False):
     mse = mean_squared_error(y_test, predicted_y)
 
     if graph:
-        xs = np.linspace(0, ceil(max(x)[0]),1000)
-        plt.plot(xs, line(model.coef_[0],model.intercept_,xs), marker='o', linewidth=1, markersize=0, color="purple", label="Linear Regression model")
+        xs = np.linspace(0, ceil(max(x)[0]), 1000)
+        plt.plot(xs, line(model.coef_[0], model.intercept_, xs), marker='o',
+                 linewidth=1, markersize=0, color="purple", label="Linear Regression model")
         plt.scatter(x, y, color="blue", s=2, label="Data Points")
-        plt.scatter(x_test, predicted_y, color="red", s=3, label="Predicted Points")
-        # *x + 
+        plt.scatter(x_test, predicted_y, color="red",
+                    s=3, label="Predicted Points")
         plt.xlabel("TB Cases")
         plt.ylabel("Life Expectancy")
         plt.title("Linear Regression of Tuberculosis Cases against Life Expectancy")
@@ -40,6 +42,8 @@ def LR_Model(data, h1, h2, graph=False):
 
     return {"MSE": mse}
 
+
 if __name__ == "__main__":
     model = LR_Model(data, "TB Cases", "Life Expectancy", True)
-    print(f"MSE of the linear model between TB cases and life expectancy: {model["MSE"]}\nHence this is not a very good model (but that wasn't really the point). This data is perhaps not the best candidate for a linear regression, by visual inspection, because there are so many countries with no TB cases but wildly different life expectancy.")
+    print(
+        f"MSE of the linear model between TB cases and life expectancy: {model["MSE"]}\nHence this is not a very good model (but that wasn't really the point). This data is perhaps not the best candidate for a linear regression, by visual inspection, because there are so many countries with no TB cases but wildly different life expectancy.")
