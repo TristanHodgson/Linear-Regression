a566c30bf43e882799b6c4fb868bd01c161309f5
Tristan Hodgson <hello@tristanhodgson.com>
2025-03-12 12:11:59 +0000

    Adding detailed comments to explain what I'm doing

--- main.py
+++ main.py
@@ -12,10 +12,65 @@ data = pd.read_csv(data_path)
 
 
 def line(a, b, x):
+    """
+    Compute the value of a line using the linear equation
+
+    $$ y = ax + b $$
+
+    Parameters:
+    -----------
+    a : float
+        The slope of the line.
+    b : float
+        The y-intercept of the line.
+    x : float or array-like
+        The value or array of values at which to evaluate the line.
+
+    Returns:
+    --------
+    float
+        The computed y value corresponding to the input x.
+    """
     return a*x+b
 
 
 def LR_Model(data, h1, h2, graph=False):
+    """
+    Build and evaluate a linear regression model.
+
+    This function splits the input dataset into training and testing subsets, fits a linear regression 
+    model using the specified independent and dependent variables, and computes key evaluation metrics 
+    including the Mean Squared Error (MSE), coefficient of determination R^2, the F-statistic, 
+    and the corresponding p-value. Optionally, it plots the regression line along with the data 
+    points and predicted values.
+
+    The F-statistic is computed by the formula:
+
+    $F = \\frac{R^2}{1-R^2} \\frac{n-p-1}{p}$
+
+    where n is the number of training samples and p=1 is the number of predictors.
+
+    Parameters:
+    -----------
+    data : pandas.DataFrame
+        The dataset containing the variables.
+    h1 : str
+        The column name in data to be used as the independent variable.
+    h2 : str
+        The column name in data to be used as the dependent variable.
+    graph : bool, optional
+        If True, displays a plot of the regression line along with the data points and the predicted points.
+        Default is False.
+
+    Returns:
+    --------
+    dict
+        A dictionary containing the following keys:
+        - "MSE": The Mean Squared Error of the predictions.
+        - "F": The computed F-statistic.
+        - "p": The p-value corresponding to the F-statistic.
+        - "r2": The coefficient of determination R^2 of the model on the training data.
+    """
     x = data[h1].values.reshape(-1, 1)
     y = data[h2].values
 
@@ -31,7 +86,7 @@ def LR_Model(data, h1, h2, graph=False):
     n = len(x_train)
     p = 1
     df = n - p - 1  # Denominator degrees of freedom
-    # F = \frac{R^2}{1-R^2} \frac{n-p-1}{p}
+    # $F = \frac{R^2}{1-R^2} \frac{n-p-1}{p}$
     F = (r_squared/(1-r_squared)) * df/p
     p_value = f.sf(F, p, df)
 
