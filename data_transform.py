65ecbf088466944c29eba4e9082508647d9a63da
Tristan Hodgson <hello@tristanhodgson.com>
2025-03-19 12:37:42 +0000

    Adding gdp to transform

--- data_transform.py
+++ data_transform.py
@@ -1,24 +1,29 @@
 import pandas as pd
 
-tb_path = "./data/incidence-of-tuberculosis-sdgs.csv"
-life_path = "./data/life-expectancy.csv"
-final_path = "./data/data.csv"
+
+def Process(renames, path):
+
+    data = pd.read_csv(path)
+    for rename in renames:
+        data.rename(columns={rename[0]: rename[1]}, inplace=True)
+    return data
 
 
-def Process():
-    tb_data = pd.read_csv(tb_path)
-    life_expectancy_data = pd.read_csv(life_path)
-    tb_data.rename(columns={
-                   "Estimated incidence of all forms of tuberculosis": "TB Cases"}, inplace=True)
-    life_expectancy_data.rename(columns={
-                                "Period life expectancy at birth - Sex: total - Age: 0": "Life Expectancy"}, inplace=True)
-
-    merged_data = pd.merge(tb_data, life_expectancy_data, on=[
+def Merge(predictors, outputs, csvPath, columns):
+    for dataSet in predictors:
+        outputs = pd.merge(dataSet, outputs, on=[
                            "Entity", "Year"], how="inner")
-    merged_data = merged_data[["TB Cases", "Life Expectancy"]].dropna()
-
-    merged_data.to_csv(final_path, index=False)
+    outputs = outputs[columns].dropna()
+    outputs.to_csv(csvPath, index=False)
+    return True
 
 
 if __name__ == "__main__":
-    Process()
+    lifeExpectancy = Process([["Period life expectancy at birth - Sex: total - Age: 0",
+                             "Life Expectancy"]], "./data/life-expectancy.csv")
+    tbCases = Process(
+        [["Estimated incidence of all forms of tuberculosis", "TB Cases"]], "./data/tb.csv")
+    gdp = Process(
+        [["GDP per capita, PPP (constant 2021 international $)", "GDP"]], "./data/gdp.csv")
+    Merge([tbCases, gdp], lifeExpectancy, "./data/data.csv",
+          ["TB Cases", "Life Expectancy"])
--- main.py
+++ main.py
@@ -35,42 +35,6 @@ def line(a, b, x):
 
 
 def LR_Model(data, h1, h2, graph=False):
-    """
-    Build and evaluate a linear regression model.
-
-    This function splits the input dataset into training and testing subsets, fits a linear regression 
-    model using the specified independent and dependent variables, and computes key evaluation metrics 
-    including the Mean Squared Error (MSE), coefficient of determination R^2, the F-statistic, 
-    and the corresponding p-value. Optionally, it plots the regression line along with the data 
-    points and predicted values.
-
-    The F-statistic is computed by the formula:
-
-    $F = \\frac{R^2}{1-R^2} \\frac{n-p-1}{p}$
-
-    where n is the number of training samples and p=1 is the number of predictors.
-
-    Parameters:
-    -----------
-    data : pandas.DataFrame
-        The dataset containing the variables.
-    h1 : str
-        The column name in data to be used as the independent variable.
-    h2 : str
-        The column name in data to be used as the dependent variable.
-    graph : bool, optional
-        If True, displays a plot of the regression line along with the data points and the predicted points.
-        Default is False.
-
-    Returns:
-    --------
-    dict
-        A dictionary containing the following keys:
-        - "MSE": The Mean Squared Error of the predictions.
-        - "F": The computed F-statistic.
-        - "p": The p-value corresponding to the F-statistic.
-        - "r2": The coefficient of determination R^2 of the model on the training data.
-    """
     x = data[h1].values.reshape(-1, 1)
     y = data[h2].values
 
@@ -92,11 +56,9 @@ def LR_Model(data, h1, h2, graph=False):
 
     if graph:
         xs = np.linspace(0, ceil(max(x)[0]), 1000)
-        plt.plot(xs, line(model.coef_[0], model.intercept_, xs), marker='o',
-                 linewidth=1, markersize=0, color="purple", label="Linear Regression model")
+        plt.plot(xs, line(model.coef_[0], model.intercept_, xs), marker='o', linewidth=1, markersize=0, color="purple", label="Linear Regression model")
         plt.scatter(x, y, color="blue", s=2, label="Data Points")
-        plt.scatter(x_test, predicted_y, color="red",
-                    s=3, label="Predicted Points")
+        plt.scatter(x_test, predicted_y, color="red", s=3, label="Predicted Points")
         plt.xlabel("TB Cases")
         plt.ylabel("Life Expectancy")
         plt.title("Linear Regression of Tuberculosis Cases against Life Expectancy")
@@ -108,4 +70,5 @@ def LR_Model(data, h1, h2, graph=False):
 
 if __name__ == "__main__":
     model = LR_Model(data, "TB Cases", "Life Expectancy")
-    print(f"Linear model between TB cases and life expectancy:\n\tr^2:\t\t{model["r2"]}\n\tMSE:\t\t{model["MSE"]}\n\tF:\t\t{model["F"]}\n\tp value:\t\t{model["p"]:.2e}\n\nHence this is not a great model but its actually by no means bad because it means that a typical distance between a predicted value and the observed value is {sqrt(model["MSE"]):.2f}.")
+    print(
+        f"Linear model between TB cases and life expectancy:\n\tr^2:\t\t{model["r2"]}\n\tMSE:\t\t{model["MSE"]}\n\tF:\t\t{model["F"]}\n\tp value:\t\t{model["p"]:.2e}\n\nHence this is not a great model but its actually by no means bad because it means that a typical distance between a predicted value and the observed value is {sqrt(model["MSE"]):.2f}.")
