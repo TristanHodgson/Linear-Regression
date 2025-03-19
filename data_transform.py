5ede3277049578110c78f287e6584a0fe9aaefc4
Tristan Hodgson <hello@tristanhodgson.com>
2025-03-19 23:58:37 +0000

    Mutiple linear regression

--- data_transform.py
+++ data_transform.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 
 def Process(renames, path):
-
     data = pd.read_csv(path)
     for rename in renames:
         data.rename(columns={rename[0]: rename[1]}, inplace=True)
@@ -11,19 +10,14 @@ def Process(renames, path):
 
 def Merge(predictors, outputs, csvPath, columns):
     for dataSet in predictors:
-        outputs = pd.merge(dataSet, outputs, on=[
-                           "Entity", "Year"], how="inner")
+        outputs = pd.merge(dataSet, outputs, on=["Entity", "Year"], how="inner")
     outputs = outputs[columns].dropna()
     outputs.to_csv(csvPath, index=False)
     return True
 
 
 if __name__ == "__main__":
-    lifeExpectancy = Process([["Period life expectancy at birth - Sex: total - Age: 0",
-                             "Life Expectancy"]], "./data/life-expectancy.csv")
-    tbCases = Process(
-        [["Estimated incidence of all forms of tuberculosis", "TB Cases"]], "./data/tb.csv")
-    gdp = Process(
-        [["GDP per capita, PPP (constant 2021 international $)", "GDP"]], "./data/gdp.csv")
-    Merge([tbCases, gdp], lifeExpectancy, "./data/data.csv",
-          ["TB Cases", "Life Expectancy"])
+    lifeExpectancy = Process([["Period life expectancy at birth - Sex: total - Age: 0","Life Expectancy"]], "./data/life-expectancy.csv")
+    tbCases = Process([["Estimated incidence of all forms of tuberculosis", "TB Cases"]], "./data/tb.csv")
+    gdp = Process([["GDP per capita, PPP (constant 2021 international $)", "GDP"]], "./data/gdp.csv")
+    Merge([tbCases, gdp], lifeExpectancy, "./data/data.csv",["TB Cases", "GDP", "Life Expectancy"])
--- main.py
+++ main.py
@@ -11,64 +11,53 @@ data_path = "./data/data.csv"
 data = pd.read_csv(data_path)
 
 
-def line(a, b, x):
-    """
-    Compute the value of a line using the linear equation
-
-    $$ y = ax + b $$
-
-    Parameters:
-    -----------
-    a : float
-        The slope of the line.
-    b : float
-        The y-intercept of the line.
-    x : float or array-like
-        The value or array of values at which to evaluate the line.
-
-    Returns:
-    --------
-    float
-        The computed y value corresponding to the input x.
-    """
-    return a*x+b
+def plane(a, b, c, x):
+    return a*x+b*y+c
 
 
-def LR_Model(data, h1, h2, graph=False):
-    x = data[h1].values.reshape(-1, 1)
-    y = data[h2].values
-
+def LR_Model(data, predictors, outputs, graph=False):
+    # Make the model
+    x = data[predictors].values
+    y = data[outputs].values
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.1, random_state=100)
-
     model = LinearRegression().fit(x_train, y_train)
-
+    # Evaluate the model
     predicted_y = model.predict(x_test)
-
     mse = mean_squared_error(y_test, predicted_y)
     r_squared = model.score(x_train, y_train)
     n = len(x_train)
-    p = 1
+    p = x_train.shape[1]
     df = n - p - 1  # Denominator degrees of freedom
     # $F = \frac{R^2}{1-R^2} \frac{n-p-1}{p}$
     F = (r_squared/(1-r_squared)) * df/p
     p_value = f.sf(F, p, df)
 
     if graph:
-        xs = np.linspace(0, ceil(max(x)[0]), 1000)
-        plt.plot(xs, line(model.coef_[0], model.intercept_, xs), marker='o', linewidth=1, markersize=0, color="purple", label="Linear Regression model")
-        plt.scatter(x, y, color="blue", s=2, label="Data Points")
-        plt.scatter(x_test, predicted_y, color="red", s=3, label="Predicted Points")
-        plt.xlabel("TB Cases")
-        plt.ylabel("Life Expectancy")
-        plt.title("Linear Regression of Tuberculosis Cases against Life Expectancy")
-        plt.legend()
-        plt.show()
+        # Plot the data points
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(x_train[:, 0], x_train[:, 1], y_train, color="blue", label="Training Data Points")
+        ax.scatter(x_test[:, 0], x_test[:, 1], y_test, color="red", label="Testing Data Points")
 
+        # Plot the prediction plane
+        xs = np.linspace(0, ceil(max(x[:, 0])), 1000)
+        ys = np.linspace(0, ceil(max(x[:, 1])), 1000)
+        xs, ys = np.meshgrid(xs, ys)
+        zs = model.intercept_ + model.coef_[0] * xs + model.coef_[1] * ys
+        ax.plot_surface(xs, ys, zs, alpha=0.5, color='purple', label='Prediction Plane')
+
+        # Make the graph pretty
+        ax.set_xlabel("TB Cases")
+        ax.set_ylabel("GDP")
+        ax.set_zlabel("Life Expectancy")
+        ax.set_title("Linear Regression of GDP and Tuberculosis Cases against Life Expectancy")
+        ax.legend()
+        plt.show()
     return {"MSE": mse, "F": F, "p": p_value, "r2": r_squared}
 
 
 if __name__ == "__main__":
-    model = LR_Model(data, "TB Cases", "Life Expectancy")
+    model = LR_Model(data, ["TB Cases", "GDP"], "Life Expectancy", graph=True)
     print(
-        f"Linear model between TB cases and life expectancy:\n\tr^2:\t\t{model["r2"]}\n\tMSE:\t\t{model["MSE"]}\n\tF:\t\t{model["F"]}\n\tp value:\t\t{model["p"]:.2e}\n\nHence this is not a great model but its actually by no means bad because it means that a typical distance between a predicted value and the observed value is {sqrt(model["MSE"]):.2f}.")
+        f"Multiple Linear model between TB cases, GDP and life expectancy:\n\tr^2:\t\t{model["r2"]}\n\tMSE:\t\t{model["MSE"]}\n\tF:\t\t{model["F"]}\n\tp value:\t\t{model["p"]:.2e}\n\nThis means that a typical distance between a predicted value and the observed value is {sqrt(model["MSE"]):.2f}.")
